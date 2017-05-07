04fecd9f06e043f42c45d2546b7308d2fe664b62
Hunter Long <info@socialeck.com>
2017-05-07 16:00:37 -0600

    first

---
+++ lib/ethereumpos/gem.rb
@@ -0,0 +1,7 @@
+require "ethereumpos/gem/version"
+
+module Ethereumpos
+  module Gem
+    # Your code goes here...
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'ethereumpos/gem'
+
+require 'minitest/autorun'
